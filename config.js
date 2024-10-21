d30876dfa3addef793ea43faa277f7fc587114d4
Denu8 <kingdenuwan07@gmail.com>
2024-10-21 10:27:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 MONGODB: process.env.MONGODB || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_4265.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_4270.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39"
